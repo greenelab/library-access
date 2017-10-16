37f2517b28bd08675be02b0ac22881077d00f8de
Jacob Levernier <jlevern@upenn.edu>
2017-10-15 21:35:08 -0400

    Re-added config. file template.

---
+++ library_management_system_downloader/downloader_configuration_file_TEMPLATE.py
@@ -0,0 +1,14 @@
+# These are parameters that stay the same from query to query (unlike the
+# doi):
+static_parameters_for_api = {}
+
+user_agent_custom_string = (
+        'This is part of a bulk download for a research '
+        'project by Jane Doe of the University of ________. '
+        'I have put a threshold on the download in order '
+        'not to cause a problem with your servers. If this '
+        'download IS causing a problem, please feel free to email me at '
+        '________, or to call me directly at ________.')
+
+api_base_url = (
+            'https://example.com/openurl')
