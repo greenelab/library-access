253e826b1db9130d61e795ee6163777e9b510383
Daniel Himmelstein <daniel.himmelstein@gmail.com>
2017-11-02 12:23:44 -0400

    TST: parametrize test_fulltext_indication
    Ignore .cache directory created by pytest

--- library_management_system_downloader/test_evaluate_api_response_for_fulltext_indication.py
+++ library_management_system_downloader/test_evaluate_api_response_for_fulltext_indication.py
@@ -1,8 +1,14 @@
+import pytest
+
 import evaluate_api_response_for_fulltext_indication as to_test
 
 
-def test_fulltext_indication():
-    assert to_test.fulltext_indication(
-            '<key id="full_text_indicator">true</key>') == 1
-
-    assert to_test.fulltext_indication('tester') == 0
+@pytest.mark.parametrize(['xml', 'fulltext'], [
+    ('<key id="full_text_indicator">true</key>', 1),
+    ('<wrapper><key id="full_text_indicator">true</key></wrapper>', 1),
+    ('<key id="full_text_indicator">false</key>', 0),
+    ('tester', 0),
+    ('', 0),
+])
+def test_fulltext_indication(xml, fulltext):
+    assert to_test.fulltext_indication(xml) == fulltext
